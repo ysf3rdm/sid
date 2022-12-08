6e32f185fc7d1b5e5bafe482d5924dc702a2e90e
Yusuf Erdem <y.erdemfutbol@hotmail.com>
2022-12-08 20:46:53 +0300

    first commit

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,12 +1,12 @@
-const ENS = artifacts.require("./ENSRegistry.sol");
-const FIFSRegistrar = artifacts.require('./FIFSRegistrar.sol');
+const BAU = artifacts.require("./BAURegistry.sol");
+const FIFSRegistrar = artifacts.require("./FIFSRegistrar.sol");
 
 // Currently the parameter('./ContractName') is only used to imply
 // the compiled contract JSON file name. So even though `Registrar.sol` is
 // not existed, it's valid to put it here.
 // TODO: align the contract name with the source code file name.
-const web3 = new (require('web3'))();
-const namehash = require('eth-ens-namehash');
+const web3 = new (require("web3"))();
+const namehash = require("eth-ens-namehash");
 
 /**
  * Calculate root node hashes given the top level domain(tld)
@@ -16,7 +16,7 @@ const namehash = require('eth-ens-namehash');
 function getRootNodeFromTLD(tld) {
   return {
     namehash: namehash.hash(tld),
-    sha3: web3.sha3(tld)
+    sha3: web3.sha3(tld),
   };
 }
 
@@ -30,22 +30,24 @@ function deployFIFSRegistrar(deployer, tld) {
   var rootNode = getRootNodeFromTLD(tld);
 
   // Deploy the ENS first
-  deployer.deploy(ENS)
+  deployer
+    .deploy(BAU)
     .then(() => {
       // Deploy the FIFSRegistrar and bind it with ENS
-      return deployer.deploy(FIFSRegistrar, ENS.address, rootNode.namehash);
+      return deployer.deploy(FIFSRegistrar, BAU.address, rootNode.namehash);
     })
-    .then(function() {
+    .then(function () {
       // Transfer the owner of the `rootNode` to the FIFSRegistrar
-      return ENS.at(ENS.address).then((c) => c.setSubnodeOwner('0x0', rootNode.sha3, FIFSRegistrar.address));
+      return BAU.at(BAU.address).then((c) =>
+        c.setSubnodeOwner("0x0", rootNode.sha3, FIFSRegistrar.address)
+      );
     });
 }
 
-module.exports = function(deployer, network) {
-  var tld = 'eth';
+module.exports = function (deployer, network) {
+  var tld = "bau";
 
-  if (network === 'dev.fifs') {
+  if (network === "dev.fifs") {
     deployFIFSRegistrar(deployer, tld);
   }
-
 };
--- truffle.js
+++ truffle.js
@@ -1,14 +1,36 @@
+const HDWalletProvider = require("@truffle/hdwallet-provider");
+const mnemonic =
+  "two fit body bottom expect stay intact turkey hazard symbol govern practice";
 module.exports = {
   networks: {
-    'dev.fifs': {
+    "dev.fifs": {
       host: "localhost",
       port: 8545,
-      network_id: "*" // Match any network id
-    }
+      network_id: "*", // Match any network id
+    },
+    testnet: {
+      provider: () =>
+        new HDWalletProvider(
+          mnemonic,
+          `https://data-seed-prebsc-1-s1.binance.org:8545`
+        ),
+      network_id: 97,
+      confirmations: 10,
+      timeoutBlocks: 200,
+      skipDryRun: true,
+    },
+    bsc: {
+      provider: () =>
+        new HDWalletProvider(mnemonic, `https://bsc-dataseed1.binance.org`),
+      network_id: 56,
+      confirmations: 10,
+      timeoutBlocks: 200,
+      skipDryRun: true,
+    },
   },
   compilers: {
     solc: {
       version: "0.7.4",
-    }
-  }
+    },
+  },
 };
